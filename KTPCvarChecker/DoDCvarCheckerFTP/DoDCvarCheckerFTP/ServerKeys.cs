44cfec90dbeadc8ca9ec618a9106a77d6c9619eb
Tony Frazier <anthony.frazier1986@gmail.com>
2022-07-08 18:41:32 -0400

    Updates to folder structure

---
+++ KTPCvarChecker/DoDCvarCheckerFTP/DoDCvarCheckerFTP/ServerKeys.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoDCvarCheckerFTP {
+    public static partial class ServerKeys {
+        public static readonly string NineteenEleven_CHI_TwentyFiveSlot_HOSTNAME = "";
+        public static readonly string NineteenEleven_CHI_TwentyFiveSlot_IP = "";
+        public static readonly string NineteenEleven_CHI_TwentyFiveSlot_PORT = "";
+        public static readonly string NineteenEleven_CHI_TwentyFiveSlot_USERNAME = "";
+        public static readonly string NineteenEleven_CHI_TwentyFiveSlot_PASSWORD = "";
+
+        public static readonly string NineteenEleven_CHIOne_HOSTNAME = "";
+        public static readonly string NineteenEleven_CHIOne_IP = "";
+        public static readonly string NineteenEleven_CHIOne_PORT = "";
+        public static readonly string NineteenEleven_CHIOne_USERNAME = "";
+        public static readonly string NineteenEleven_CHIOne_PASSWORD = "";
+
+        public static readonly string NineteenEleven_CHIThree_HOSTNAME = "";
+        public static readonly string NineteenEleven_CHIThree_IP = "";
+        public static readonly string NineteenEleven_CHIThree_PORT = "";
+        public static readonly string NineteenEleven_CHIThree_USERNAME = "";
+        public static readonly string NineteenEleven_CHIThree_PASSWORD = "";
+
+        public static readonly string NineteenEleven_DALOne_HOSTNAME = "";
+        public static readonly string NineteenEleven_DALOne_IP = "";
+        public static readonly string NineteenEleven_DALOne_PORT = "";
+        public static readonly string NineteenEleven_DALOne_USERNAME = "";
+        public static readonly string NineteenEleven_DALOne_PASSWORD = "";
+
+        public static readonly string NineteenEleven_NYOne_HOSTNAME = "";
+        public static readonly string NineteenEleven_NYOne_IP = "";
+        public static readonly string NineteenEleven_NYOne_PORT = "";
+        public static readonly string NineteenEleven_NYOne_USERNAME = "";
+        public static readonly string NineteenEleven_NYOne_PASSWORD = "";
+
+        public static readonly string CORYBBJ_HOSTNAME = "";
+        public static readonly string CORYBBJ_IP = "";
+        public static readonly string CORYBBJ_PORT = "";
+        public static readonly string CORYBBJ_USERNAME = "";
+        public static readonly string CORYBBJ_PASSWORD = "";
+
+        public static readonly string MTP_NY_HOSTNAME = "";
+        public static readonly string MTP_NY_IP = "";
+        public static readonly string MTP_NY_PORT = "";
+        public static readonly string MTP_NY_USERNAME = "";
+        public static readonly string MTP_NY_PASSWORD = "";
+
+        public static readonly string MTP_AL_HOSTNAME = "";
+        public static readonly string MTP_AL_IP = "";
+        public static readonly string MTP_AL_PORT = "";
+        public static readonly string MTP_AL_USERNAME = "";
+        public static readonly string MTP_AL_PASSWORD = "";
+
+        public static readonly string Thunder_CHI_HOSTNAME = "";
+        public static readonly string Thunder_CHI_IP = "";
+        public static readonly string Thunder_CHI_PORT = "";
+        public static readonly string Thunder_CHI_USERNAME = "";
+        public static readonly string Thunder_CHI_PASSWORD = "";
+
+        public static readonly string Thunder_NY_HOSTNAME = "";
+        public static readonly string Thunder_NY_IP = "";
+        public static readonly string Thunder_NY_PORT = "";
+        public static readonly string Thunder_NY_USERNAME = "";
+        public static readonly string Thunder_NY_PASSWORD = "";
+    }
+}
